2e2d44cf4cd07e126c60f0b28253d39224bf0dff
Пастухов Кирилл <Kirill.pastukhov.90@mail.ru>
2023-04-23 18:51:34 +0700

    Добавлены изменения в файлы .h

---
+++ src/libgeometry/lexer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <parser.h>
+
+#define CIRCLE_START 6
+#define TRIANGLE_START 8
+
+void addSpaes(int);
+void skipSpaces(char *, Figure *, int *);
+void fillErrorString(char *, int, char *);
+double getValue(char *, int *, Figure *, char, char *);
+int circleHandler(char *, Figure *, char *);
+int trianglehandler(char *, Figure *, char *);
+int stringHandler(char*, Figure *, char *);
