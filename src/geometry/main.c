341109d0d907dbb417278f900c7110d53948e375
Shepherdboo <124231831+Shepherdboo@users.noreply.github.com>
2023-04-28 19:50:24 +0700

    Исправлены ошибки

--- src/geometry/main.c
+++ src/geometry/main.c
@@ -2,18 +2,20 @@
 
 #include <parser.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    if (argv[1] == NULL) {
-        printf("input 'circle(point, number)' "
+    if (argv[1] == NULL)
+    {
+        printf("The input is a file with format lines: 'circle(point, number)' "
                "or 'triangle((point, point, point, point))'\n");
         return 1;
     }
 
     Figure figures[MIN_ELEMENTS];
     char errmsg[MAX_ELEMENTS];
-    int count = readFile(argv[1],figures, errmsg);
+    int count = readFile(argv[1], figures, errmsg);
     printFigures(figures, count);
 
     return 0;
 }
+
